650251c66478e3a471072439a5001dd4695b72a9
Vinh Ha <phuocvinh.ha@gmail.com>
2025-06-14 16:59:29 +0700

    Fix more path issues for Cloudflare Pages deployment

--- fix-paths.sh
+++ fix-paths.sh
@@ -7,4 +7,13 @@ find . -type f -name "*.html" -exec sed -i '' 's|<base href="/online-tools/">|<b
 find . -type f -name "*.html" -exec sed -i '' 's|src="images/|src="/images/|g' {} +
 find . -type f -name "*.html" -exec sed -i '' 's|href="images/|href="/images/|g' {} +
 find . -type f -name "*.html" -exec sed -i '' 's|src="js/|src="/js/|g' {} +
+find . -type f -name "*.html" -exec sed -i '' 's|href="js/|href="/js/|g' {} +
 find . -type f -name "*.html" -exec sed -i '' 's|href="css/|href="/css/|g' {} +
+find . -type f -name "*.html" -exec sed -i '' 's|content="https://emn178.github.io/online-tools/|content="/|g' {} +
+
+# Fix paths in script tags
+find . -type f -name "*.html" -exec sed -i '' 's|<script src="js/|<script src="/js/|g' {} +
+find . -type f -name "*.html" -exec sed -i '' 's|<link rel="stylesheet" href="css/|<link rel="stylesheet" href="/css/|g' {} +
+
+# Remove any double slashes in paths (except for http://)
+find . -type f -name "*.html" -exec sed -i '' 's|([^:])//|\\1/|g' {} +
